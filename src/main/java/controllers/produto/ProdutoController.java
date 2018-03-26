17a89872259b5b3e81bfbfbc44df17e4013d67f3
Jonatan José Soares <jonatan.jsoares@gmail.com>
2018-03-26 19:08:27 -0300

    Inserção dos Controllers ProdutoAdicional, Produto e ProdutoCategoria ao projeto

---
+++ src/main/java/controllers/produto/ProdutoController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers.produto;
+
+import beans.produto.ProdutoBean;
+
+/**
+ *
+ * @author JONATAN
+ */
+public class ProdutoController {
+        
+    }
+   
