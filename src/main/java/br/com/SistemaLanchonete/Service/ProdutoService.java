957cccaa21ea45f652f6bbe0f0d2f57d99193fa1
= <jonsoar94@gmail.com>
2018-04-19 00:11:27 -0300

    Criação do ProdutoService (Todo: Metodo save, delete e select)

---
+++ src/main/java/br/com/SistemaLanchonete/Service/ProdutoService.java
@@ -0,0 +1,26 @@
+package br.com.SistemaLanchonete.Service;
+
+import br.com.SistemaLanchonete.Domain.ProdutoBean;
+
+public class ProdutoService {
+	private String retorno = "";
+
+	public String save(ProdutoBean produto) {
+		// TODO
+		return retorno;
+	}
+
+	public void delete(int cdPessoa) {
+		// TODO
+		
+	}
+	
+	public ProdutoBean select(int cdPessoa) {
+		// TODO
+		return null;
+	}
+
+	
+
+
+}
