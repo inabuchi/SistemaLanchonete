1606b12fd19ce4ac8062ba5e61d429daadaf441c
evieira1472 <everton1472@hotmail.com>
2018-05-02 22:52:36 -0300

    cadastros js

--- src/main/webapp/controller/produto/cadastroProduto.js
+++ src/main/webapp/controller/produto/cadastroProduto.js
@@ -1,31 +1,53 @@
-const cdCategoria = $('#codCategoria').val();
-const descRefProduto = $('#refProd').val();
-const descProduto = $('#descProd').val();
-const vlProduto = $('#valorProd').val();
-const isAtivo = $('#prodAtivo').val() == 'S';
-
-const validarProduto = () => {
-	return true;
-};
-
-const salvarProduto = () => {
-	const params = {
-		cdCategoria,
-		descRefProduto,
-		descProduto,
-		isAtivo,
-		vlProduto
-	};
-	actionRequestService('http://localhost:8080/SistemaLanchonete/services/produtos/produto', params).done(data => {
-		alert('Produto cadastrado com sucesso');
-		$(location).attr('href','ConsultaProduto.html');
-	}).fail(data => {
-		alert('Erro na requisição. Erro: '+ data.status + ' ' + data.statusText);
-	});
-};
-
-
 $(document).ready(() =>{
+	const cdCategoria = $('#codCategoria').val();
+	const descRefProduto = $('#refProd').val();
+	const descProduto = $('#descProd').val();
+	const vlProduto = $('#valorProd').val();
+	const isAtivo = $('#prodAtivo').val() == 'S';
+	
+	const validarProduto = () => {
+		return true;
+	};
+
+	const salvarProduto = () => {
+		const params = {
+			cdCategoria,
+			descRefProduto,
+			descProduto,
+			isAtivo,
+			vlProduto
+		};
+		$.ajax({
+	    	headers: {
+	            'Accept': 'application/json',
+	            'Content-Type': 'application/json'
+	        },
+	        type: 'POST'
+	        , data: JSON.stringify(params)
+	        , dataType: 'json' //
+	        , url: 'http://localhost:8080/SistemaLanchonete/services/produtos/produto'
+	        , statusCode: {
+	        	200: ()=>{
+	        		debugger;
+	        		alert("Produto cadastrado com sucesso!");
+	                $(location).attr('href','ConsultaProduto.html');
+	        	}, 
+	        	404: ()=>{
+	        		debugger;
+	        		alert('Not Found');
+	        	},
+	        	415: ()=> {
+	        		debugger;
+	        		alert('Não suportado')
+	        	},
+	        	500: () => {
+	        		debugger;
+	        		alert('ErroInterno');
+	        	}
+	        }
+	    });
+	};
+
 	$('#btnSave').click(() => {
 		salvarProduto();
 	});
