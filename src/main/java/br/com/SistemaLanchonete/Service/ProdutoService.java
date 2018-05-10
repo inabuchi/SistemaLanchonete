1c68369a6e2dcdcf30cedcf4fc1eee8e501e2879
Jonatan José Soares <jonsoar94@gmail.com>
2018-05-10 18:30:17 -0300

    Commit da Classe ProdutoService

--- src/main/java/br/com/SistemaLanchonete/Service/ProdutoService.java
+++ src/main/java/br/com/SistemaLanchonete/Service/ProdutoService.java
@@ -1,7 +1,6 @@
 package br.com.SistemaLanchonete.Service;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import br.com.SistemaLanchonete.Domain.ProdutoBean;
 import br.com.SistemaLanchonete.Repository.BDException;
@@ -12,53 +11,73 @@ public class ProdutoService {
 
 	private static String retorno = "";
 	GenericDAO<ProdutoBean> produtoDAO = new GenericDAO<ProdutoBean>();
-	Class<ProdutoBean> produtoClasse = ProdutoBean.class;
+	Class<ProdutoBean> produtoBean;
 
-	public String save(ProdutoBean produto) throws BDException {
-		if (produto.getCdProduto() == 0) {
-			try {
-				produtoDAO.save(produto, 0);
-			} catch (BDException e) {
-				throw new BDException("Erro ao Salvar dados no banco" + e.getMessage(), EErrosBD.ATUALIZA_DADO);
+	public String save(ProdutoBean produto, int id) throws BDException {
+		boolean resultado = verificaDescricaoECategoria(produto);
+		if(resultado) {
+			if(id == 0 ) {
+				try {
+					produtoDAO.save(produto, 0);
+				} catch (Exception e) {
+					throw new BDException("Erro ao Salvar dados no banco" + e.getMessage(), EErrosBD.ATUALIZA_DADO);
+				}
+				
+				retorno = "Produto " + produto.getCdProduto() + " - " + produto.getDsProduto() + " adicionado com sucesso!";
+				
+			} else {
+				try {
+					produtoDAO.save(produto, id);
+				} catch (BDException e) {
+					throw new BDException("Erro ao atualizar Produto " + produto.getCdProduto() + e.getMessage(), EErrosBD.ATUALIZA_DADO);
 
+				}
+				
+				retorno = "Produto " + produto.getCdProduto() + " - " + produto.getDsProduto() + " atualizado com sucesso!";
 			}
-			retorno = "Dados salvos com sucesso na tabela";
-		} else {
-			try {
-				produtoDAO.save(produto, produto.getCdProduto());
-			} catch (BDException e) {
-				throw new BDException("Erro ao atualizar Produto " + produto.getCdProduto() + e.getMessage(),
-						EErrosBD.ATUALIZA_DADO);
-
-			}
-			retorno = "Dados atualizados com sucesso na tabela";
 		}
-		return retorno;
+		return retorno = "Descrição de Produto & Categoria não preenchidos!";
 	}
-
+				
 	public String remove(ProdutoBean produto) throws BDException {
-		ProdutoBean produtoRetorna = produtoDAO.findById(produtoClasse, produto.getCdProduto());
+		ProdutoBean produtoRetorna = produtoDAO.findById(produtoBean, produto.getCdProduto());
 		try {
-			;
-			produtoDAO.remove(produtoClasse, produtoRetorna.getCdProduto());
+			produtoDAO.remove(produtoBean, produtoRetorna.getCdProduto());
 			retorno = "Dados removidos com sucesso na tabela";
 		} catch (Exception e) {
-			throw new BDException("Erro ao remover : " + produto.getCdProduto() + e.getMessage(), EErrosBD.EXCLUI_DADO);
+			throw new BDException("Erro ao remover : " + produto.getCdProduto()  + e.getMessage(), EErrosBD.EXCLUI_DADO);
 		}
 		return retorno;
 	}
-
-	public ProdutoBean findById(ProdutoBean produto) {
-		return produtoDAO.findById(produtoClasse, produto.getCdProduto());
-
-	}
-
-	public ArrayList<ProdutoBean> findLike(ProdutoBean produto) {
-		ArrayList<ProdutoBean> lista = new ArrayList<ProdutoBean>();
-		List<ProdutoBean> lista2 = produtoDAO.findLike(produtoClasse, produto);
-		for (ProdutoBean model2 : lista2) {
-			lista.add(model2);
+	
+	public ProdutoBean findById(ProdutoBean produto) throws BDException {
+		try {
+			return produtoDAO.findById(produtoBean, produto.getCdProduto());
+		} catch (Exception e) {
+			throw new BDException("Erro ao buscar produto " + produto.getCdProduto() + " - " + produto.getDsProduto() + e.getMessage(), EErrosBD.CONSULTA_DADO);
 		}
-		return lista;
+	}
+	
+	public ArrayList<ProdutoBean> findLike(ProdutoBean produto) throws BDException {
+		try {
+			ArrayList<ProdutoBean> listaProdutos = new ArrayList<ProdutoBean>();
+			return listaProdutos;
+		} catch (Exception e) {
+			throw new BDException("Erro ao buscar produto " + produto.getCdProduto() + " - " + produto.getDsProduto() + e.getMessage(), EErrosBD.CONSULTA_DADO);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param produtos
+	 * @return true se descri��o de produto e categoria foram preenchidos. Caso contr�rio, falso.
+	 */
+	public boolean verificaDescricaoECategoria(ProdutoBean produtos) {
+		if(produtos.getDsProduto() != null && produtos.getDsProduto().trim().length() > 0 &&
+		   produtos.getCategoria() != null && produtos.getCategoria().toString().trim().length() > 0) {
+			return true;
+		}
+		return false;
 	}
 }
+
